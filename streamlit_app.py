631a11a10a07e2a04bc71d8a1ca21cc07fbaa145
Aparna <121015464+aparna-1593@users.noreply.github.com>
2022-12-26 11:45:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
 add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit)
-streamlit.write('Thanks for adding 'add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
